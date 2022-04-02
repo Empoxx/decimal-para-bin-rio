fe3c11d368db6f0f2de2da70d2d02275acf67d1a
Empoxx <euteodeioicaro@outlook.com>
2022-04-02 19:08:44 -0300

    First commit

---
+++ import binario.py
@@ -0,0 +1,29 @@
+import re
+
+numero_decimal = int(input())
+numero_da_potencia = 0
+numero_multiplicado = 0
+numero_lista = []
+numero_somado = []
+lista_binario = []
+
+
+while numero_multiplicado * 2 <= numero_decimal:
+    numero_multiplicado = 2 ** numero_da_potencia
+    numero_lista.append(numero_multiplicado)
+    numero_da_potencia = numero_da_potencia + 1
+
+numero_lista.reverse()
+
+#print(numero_lista)
+print(f"O número decimal escolhido foi {numero_decimal}")
+for numeros in numero_lista:
+    if sum(numero_somado) + numeros <= numero_decimal:
+        lista_binario.append("1")
+        numero_somado.append(numeros)
+    else:
+        lista_binario.append("0")
+
+print(f"O resultado do número decimal {numero_decimal} é:", sum(numero_somado))
+print(numero_somado)
+print(''.join(lista_binario))
